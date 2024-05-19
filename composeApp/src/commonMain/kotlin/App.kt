93b4d55df10d555dcfec67c7ce8cdcf7839fdad6
mejdi14 <mejdiclubiste@gmail.com>
2024-05-19 22:28:59 +0200

    prepare initial component

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,19 +1,30 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import stickersswing.composeapp.generated.resources.Res
 import stickersswing.composeapp.generated.resources.compose_multiplatform
+import stickersswing.composeapp.generated.resources.heart
+import stickersswing.composeapp.generated.resources.lightning
+import stickersswing.composeapp.generated.resources.lolippop
+import stickersswing.composeapp.generated.resources.playboy
+import stickersswing.composeapp.generated.resources.watch_this
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -21,17 +32,17 @@ import stickersswing.composeapp.generated.resources.compose_multiplatform
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        Row(
+            Modifier.fillMaxSize().padding(bottom = 100.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.Bottom
+        ) {
+
+            Image(painterResource(Res.drawable.heart), null, modifier = Modifier.size(70.dp))
+            Image(painterResource(Res.drawable.lightning), null, modifier = Modifier.size(70.dp))
+            Image(painterResource(Res.drawable.lolippop), null, modifier = Modifier.size(70.dp))
+            Image(painterResource(Res.drawable.playboy), null, modifier = Modifier.size(70.dp))
+            Image(painterResource(Res.drawable.watch_this), null, modifier = Modifier.size(70.dp))
         }
     }
 }
